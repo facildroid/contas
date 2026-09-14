d1b4328d8d04ea170fd2439fdc1ae27157623d34
facildroid <79875451+facildroid@users.noreply.github.com>
2026-09-14 16:25:15 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,28 @@ const mensagem = document.getElementById("mensagem");
 botao.addEventListener("click", function () {
     mensagem.textContent = "Olá Mundo!";
 });
+
+
+const URL_PLANILHA =
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTZbkWAgGCVath4Xue9VwDiSrzrtzuccvn28zXV-MLDYlRFXkpEfrcLYQeu0-dOtdPVynaUM0JgmkGs/pub?gid=0&single=true&output=csv";
+
+async function carregarDados() {
+
+    try {
+        const resposta = await fetch(URL_PLANILHA);
+
+        if (!resposta.ok) {
+            throw new Error("Não foi possível acessar a planilha.");
+        }
+
+        const texto = await resposta.text();
+
+        console.log(texto);
+
+    } catch (erro) {
+        console.error("Erro:", erro);
+    }
+}
+
+carregarDados();
+
